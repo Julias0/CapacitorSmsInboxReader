1cef55beb602fdd2c7c3b0889f40f3886b4122f5
aiyush <aiyush.dhar@fyle.in>
2021-01-24 23:02:49 +0530

    Tested and finished implementation for android

--- android/src/main/java/com/dhar/smsinboxreader/SmsInboxReader.java
+++ android/src/main/java/com/dhar/smsinboxreader/SmsInboxReader.java
@@ -4,11 +4,14 @@ import android.Manifest;
 import android.database.Cursor;
 import android.net.Uri;
 
+import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+
+import org.json.JSONException;
 
 import java.util.ArrayList;
 
@@ -28,32 +31,24 @@ public class SmsInboxReader extends Plugin {
     }
 
     @PluginMethod
-    public void getAllMessages(PluginCall call) {
+    public void getAllMessages(PluginCall call) throws JSONException {
         Cursor cursor = getContext().getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
-        ArrayList smsArray = new ArrayList<Sms>(cursor.getCount());
 
-        if (cursor.moveToFirst()) { // must check the result to prevent exception
-            do {
-               for(int idx = 0; idx < cursor.getColumnCount(); idx++)
-               {
-                   smsArray.set(idx, new Sms(
-                           cursor.getString(cursor.getColumnIndexOrThrow("address")),
-                           cursor.getString(cursor.getColumnIndexOrThrow("body"))
-                   ));
-               }
-            } while (cursor.moveToNext());
-        } else {
-           // empty box, no SMS
+        ArrayList smsArray = new ArrayList<JSObject>();
+
+        if (cursor.moveToFirst()) {
+            for (int i = 0; i < cursor.getCount(); i++) {
+                JSObject currentSmsObject = new JSObject();
+                currentSmsObject.put("from",  cursor.getString(cursor.getColumnIndexOrThrow("address")));
+                currentSmsObject.put("message", cursor.getString(cursor.getColumnIndexOrThrow("body")));
+                smsArray.add(currentSmsObject);
+                cursor.moveToNext();
+            }
         }
 
         JSObject smsArrayContainer = new JSObject();
 
-
-        ArrayList smsJsArray = new ArrayList<JSObject>(cursor.getCount());
-        for (int i = 0; i < cursor.getCount(); i++) {
-            smsJsArray.set(i, ((Sms)smsArray.get(i)).getJson());
-        }
-        smsArrayContainer.put("sms", smsJsArray);
+        smsArrayContainer.put("sms", new JSArray(smsArray.toArray()));
         smsArrayContainer.put("count", cursor.getCount());
 
         call.success(smsArrayContainer);
